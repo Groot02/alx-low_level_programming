29c3d0eec905dd16238ffb3b1868d90dc9650d2f
Idudu12 <idudusolomon204@gmail.com>
2022-10-17 10:29:10 -0700

    program that prints all single digits starting from 0 to 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit denary numbers with putchar
+ * Return: returns 0
+ * ASCII numbers representing the digts were used
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
