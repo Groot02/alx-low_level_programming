f6993f94b8740ae20754aaee87abeb00fdd9f316
Idudu12 <idudusolomon204@gmail.com>
2022-10-18 06:03:49 -0700

    Write a program that prints all possible combinations of single

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
-	putchar(i);
-	if (i != 57)
-	putchar(44);
-	putchar(32);
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
